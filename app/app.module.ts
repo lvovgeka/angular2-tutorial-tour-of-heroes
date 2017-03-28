b883994200d73b45f892296be5bd880558c7668f
Evgeny Lvov <lvovgeka@gmail.com>
2017-03-28 17:18:50 +0800

    TOUR OF HEROES READY:)

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,22 +1,45 @@
 import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser'; 
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HeroDetailComponent } from './hero-detail.component';
+import { HttpModule }    from '@angular/http';
 
+import { AppRoutingModule }   from './app-routing.module';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent }     from './heroes.component';
+import { DashboardComponent }     from './dashboard.component';
 import { AppComponent }  from './app.component';
+import { HeroSearchComponent }  from './hero-search.component';
+import { HeroService }  from './hero.service';
+
+
+
 
 @NgModule({
-  imports:      [ 
-     BrowserModule, 
-     FormsModule 
-  ],
-  declarations: [ 
-     AppComponent,
-     HeroDetailComponent 
-  ],
-  bootstrap:    [ 
-     AppComponent 
-  ]
+    imports:      [
+       BrowserModule,
+       FormsModule,
+        HttpModule,
+        AppRoutingModule,
+        InMemoryWebApiModule.forRoot(InMemoryDataService)
+    ],
+    declarations: [
+       AppComponent,
+       HeroDetailComponent,
+        DashboardComponent,
+        HeroesComponent,
+        HeroSearchComponent
+    ],
+    providers: [
+        HeroService
+    ],
+    bootstrap:    [
+       AppComponent
+    ]
 })
 export class AppModule { }
 //https://angular.io/docs/ts/latest/tutorial/toh-pt5.html
